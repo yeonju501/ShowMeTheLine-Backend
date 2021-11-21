f43beacca9e05d7a6d9e9db93ab3148f01df36b0
고우영 <wooyeongs2@gmail.com>
2021-11-21 20:35:34 +0900

    commit

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:movie_pk>/', views.movie_detail),
     path('<int:movie_pk>/review/', views.review_list_create),
     path('<int:movie_pk>/review/<int:review_pk>/', views.review_update_delete),
+    path('bestFive/', views.bestFive),
 ]
